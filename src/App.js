18407a2062838fcd13e01b43568c04fa2eb81bf9
Marvin Mango <marvinke2001@gmail.com>
2025-04-26 18:40:21 +0300

    Testing Forms

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import { useState } from "react";
 
 function App() {
   const [pepperoniIsChecked, setPepperoniIsChecked] = useState(false);
+  const [size, setSize] = useState("small");
+  const [contactInfo, setContactInfo] = useState("");
+  const [orderIsSubmitted, setOrderIsSubmitted] = useState(false);
 
   const togglePepperoni = (e) => setPepperoniIsChecked(e.target.checked);
+  const selectSize = (e) => setSize(e.target.value);
+  const updateContactField = (e) => setContactInfo(e.target.value);
+  const submitOrder = (e) => {
+    e.preventDefault();
+    setOrderIsSubmitted(true);
+  };
 
   return (
     <div>
       <h1>Place an Order</h1>
-      <form>
+
+      <p>
+        Your selection: {size} {pepperoniIsChecked ? "pepperoni" : "cheese"}
+      </p>
+
+      <form onSubmit={submitOrder}>
+        {/* Toppings */}
         <div>
           <h3>Toppings</h3>
           <input
@@ -20,7 +35,37 @@ function App() {
           />
           <label htmlFor="pepperoni">Add pepperoni</label>
         </div>
+
+        {/* Size */}
+        <div>
+          <h3>Size</h3>
+          <label htmlFor="select-size">Select size: </label>
+          <select id="select-size" value={size} onChange={selectSize}>
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+          </select>
+        </div>
+
+        {/* Contact Info */}
+        <div>
+          <h3>Contact Info</h3>
+          <label htmlFor="email">Enter your email address: </label>
+          <input
+            type="text"
+            id="email"
+            value={contactInfo}
+            placeholder="email address"
+            onChange={updateContactField}
+          />
+        </div>
+
+        {/* Submit button */}
+        <button type="submit">Submit Order</button>
       </form>
+
+      {/* Thank you message */}
+      {orderIsSubmitted ? <h2>Thanks for your order!</h2> : null}
     </div>
   );
 }
